1d1cfaef624c03ad95c90752e54e5d8b80606e2a
Qifeng Hu <117861310+QifengHu@users.noreply.github.com>
2024-09-23 15:46:10 -0400

    Add files via upload

--- inverse/functionally_graded/losses.py
+++ inverse/functionally_graded/losses.py
@@ -23,25 +23,21 @@ def s_exact(x,y):
     return 4. * torch.exp(-0.1 * y)
 
 def physics_loss(model,x,y):
-    # for interface 2d tensor data
-    if torch.isnan(x).any():
-        return torch.zeros_like(x)
-    elif x.shape[1] != 1:
-        x = x.T.reshape(-1,1)
-        y = x.T.reshape(-1,1)
-    U         = model(x,y) # U contains T, k
+    U         = model(x,y)
     u         = U[:,0][:,None]
     k         = U[:,1][:,None]
     u_x,u_y   = torch.autograd.grad(u.sum(),(x,y),create_graph=True,retain_graph=True)
-    ku_xx     = torch.autograd.grad((k*u_x).sum(),x,create_graph=True,retain_graph=True)[0]
-    ku_yy     = torch.autograd.grad((k*u_y).sum(),y,create_graph=True,retain_graph=True)[0]
+    flux_x    = k * u_x
+    flux_y    = k * u_y
+    u_xx      = torch.autograd.grad(flux_x.sum(),x,create_graph=True,retain_graph=True)[0]
+    u_yy      = torch.autograd.grad(flux_y.sum(),y,create_graph=True,retain_graph=True)[0]
     s         = s_exact(x,y)
-    loss      = (ku_xx + ku_yy - s).pow(2)
-    return loss
+    loss      =  (u_xx + u_yy - s).pow(2) 
+    return torch.mean(loss).reshape(1, 1)
 
 def boundary_loss(model,x,y):
     if torch.isnan(x).any():
-        return torch.zeros_like(x)
+        return torch.zeros(1,1).to(device), torch.zeros(1,1).to(device)
     else:
         U       = model(x,y)
         u       = U[:,0][:,None]
@@ -49,49 +45,35 @@ def boundary_loss(model,x,y):
 
         u_bc    = u_exact(x,y)
         k_bc    = k_exact(x,y)
-        
+
         e1      = (u - u_bc).pow(2)
         e2      = (k - k_bc).pow(2)
-        return e2
+        return torch.mean(e1).reshape(1, 1), torch.mean(e2).reshape(1, 1)
 
-def avg_if_loss(Robin, model, x, y, u_adj, k_adj, du_adj):
-    if torch.isnan(x).any():
-        return torch.zeros_like(x)
-    else:
-        n, num_interface = x.shape
-        avg_loss = torch.zeros(num_interface, 1, device=device)
-        
-        for i in range(num_interface):
-            x_col = x[:, i].reshape(-1, 1)
-            y_col = y[:, i].reshape(-1, 1)
-            U_col = model(x_col, y_col)
-            u_col = U_col[:,0][:,None]
-            k_col = U_col[:,1][:,None]
+def avg_if_loss(Alpha, model, x, y, u_adj, k_adj, dudn_adj, dudt_adj):
+    n, num_interface = x.shape
+    avg_loss = torch.zeros(num_interface, 1, device=device)
 
-            all_x_same = torch.all(x_col.eq(x_col[0]))
-            if all_x_same: #column has the same x
-                du_col = k_col * torch.autograd.grad(u_col.sum(), x_col, create_graph=True)[0]
-            else:
-                du_col = k_col * torch.autograd.grad(u_col.sum(), y_col, create_graph=True)[0]
+    for i in range(num_interface):
+        x_col = x[:, i].reshape(-1, 1)
+        y_col = y[:, i].reshape(-1, 1)
+        U_col = model(x_col, y_col)
+        u_col = U_col[:,0][:,None]
+        k_col = U_col[:,1][:,None]
+        u_x_col,u_y_col   = torch.autograd.grad(u_col.sum(),(x_col,y_col),create_graph=True,retain_graph=True)
 
-            N    = du_col - du_adj[:, i].reshape(-1, 1) # interface neumann residual
-            D    = u_col - u_adj[:, i].reshape(-1, 1)  # interface dirichlet residual - for temperature
-            
-            # Robin[0]: Dirichlet; [1]: Neumann
-            avg_loss[i] = ( Robin[0].pow(2) * D.pow(2) ).mean() + (Robin[1] * N).pow(2).mean() 
-        return torch.mean(avg_loss).reshape(-1,1)
+        all_x_same = torch.all(x_col.eq(x_col[0]))
+        if all_x_same: #column has the same x
+            dudn_col = k_col * u_x_col
+            dudt_col = u_y_col
+        else:
+            dudn_col = k_col * u_y_col
+            dudt_col = u_x_col
 
-def measurement_loss(model,x,y):
-    if torch.isnan(x).any():
-        return torch.zeros_like(x)
-    else:
-        U       = model(x,y)
-        u       = U[:,0][:,None]
-        k       = U[:,1][:,None]
+        G    = u_col - u_adj[:, i].reshape(-1, 1)  # interface dirichlete residual
+        F    = dudn_col - dudn_adj[:, i].reshape(-1, 1) # interface flux residual
+        T    = dudt_col - dudt_adj[:, i].reshape(-1, 1) # interface tangential derivative
+        Gk   = k_col - k_adj[:, i].reshape(-1, 1)  # interface dirichlete residual for k
+        avg_loss[i] =  (Alpha[0] * G).pow(2).mean() + (Alpha[1] * F).pow(2).mean() + (Alpha[2] * T).pow(2).mean() + (Alpha[3] * Gk).pow(2).mean()
+    return torch.mean(avg_loss).reshape(-1,1)
 
-        u_e     = u_exact(x,y)
-        k_e     = k_exact(x,y)
-
-        e1      = (u - u_e).pow(2)
-        e2      = (k - k_e).pow(2)
-        return e1, e2
